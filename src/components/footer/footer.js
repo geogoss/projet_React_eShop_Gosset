d7e2e770bd72d471df01775aa35240355e750a6a
Nassim Chouirfa <nassim.chouirfa@gmail.com>
2022-02-03 02:14:42 +0100

    ajustement footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -80,7 +80,7 @@ import styled from 'styled-components';
    
 export const Box = styled.div`
   padding: 80px 60px;
-  background: black;
+  background: rgb(236, 209, 157);
   position: absolute;
   bottom: 0;
   width: 100%;
